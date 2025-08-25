bec129bd8bb20411f3934a8d0430598e628413ba
KatIsCoding <fabrigoru@gmail.com>
2025-08-25 13:22:53 -0600

    Generation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 #!/usr/bin/env bun
 
+import type { Casing } from "./internal/validations/common";
+import { introspectPostgres } from "./introspect";
 import { drizzleConfigFromFile } from "./utils";
 
 const conf = await drizzleConfigFromFile();
-console.log(conf);
+const casingConv: Casing = conf.casing === "snake_case" ? "preserve" : "camel";
+let schemaFilter: string[] = [];
+if (typeof conf.schemaFilter === "string") {
+	schemaFilter = [conf.schemaFilter];
+} else {
+	schemaFilter = conf.schemaFilter;
+}
+await introspectPostgres(
+	casingConv,
+	conf.out || "./db",
+	conf.breakpoints,
+	conf.dbCredentials,
+	conf.tablesFilter && typeof conf.tablesFilter === "string"
+		? [conf.tablesFilter]
+		: [],
+	schemaFilter,
+	"none",
+	undefined,
+);
